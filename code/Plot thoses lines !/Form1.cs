3910d8ca6be7c97933f222cc2ccf4867ac3c513c
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-12 16:30:17 +0200

    chore (code) : Recherche de comment importer un .CSV pour les data
    [25][WIP]

--- code/Plot thoses lines !/Form1.cs
+++ code/Plot thoses lines !/Form1.cs
@@ -19,12 +19,30 @@ namespace Plot_thoses_lines__
         {
             InitializeComponent();
 
-            // Initialisation du Chart
+            // Création d'une série temporelle
+            var serie = new Series("Mesures");
+            serie.ChartType = SeriesChartType.Line; // Ligne pour le temps
+            serie.XValueType = ChartValueType.DateTime; // Axe X = DateTime
+
+            // Ajouter quelques données fictives
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-4), 10);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-3), 20);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-2), 15);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-1), 25);
+            serie.Points.AddXY(DateTime.Now, 18);
+
+            // Ajouter la série au Chart
             chart1.Series.Clear();
+            chart1.Series.Add(serie);
+
+            // Configurer les axes
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss"; // Format des heures
             chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.LightGray;
             chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.LightGray;
 
-            chart1.ChartAreas[0].AxisX.LabelStyle.Angle = -45; // Incliner les labels si les années sont longues
+            // Optionnel : défilement/zoom
+            chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
