756ff4098767cade66afde53ff6f18470202a38d
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-12 16:28:43 +0200

    feat (Code) : Essaie de faire que les .CSV peuvent être importe
    [110][WIP]
    Sans réussite :,)

--- code/Plot thoses lines !/Form1.Designer.cs
+++ code/Plot thoses lines !/Form1.Designer.cs
@@ -32,6 +32,8 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnChargerCSV = new System.Windows.Forms.Button();
+            this.ChargerCSV = new System.Windows.Forms.OpenFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,22 +43,38 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(-1, 53);
+            this.chart1.Location = new System.Drawing.Point(-4, 59);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(802, 396);
+            this.chart1.Size = new System.Drawing.Size(805, 392);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // btnChargerCSV
+            // 
+            this.btnChargerCSV.Location = new System.Drawing.Point(686, 12);
+            this.btnChargerCSV.Name = "btnChargerCSV";
+            this.btnChargerCSV.Size = new System.Drawing.Size(102, 37);
+            this.btnChargerCSV.TabIndex = 1;
+            this.btnChargerCSV.Text = "Import a Chart";
+            this.btnChargerCSV.UseVisualStyleBackColor = true;
+            this.btnChargerCSV.Click += new System.EventHandler(this.btnChargerCSV_Click);
+            // 
+            // ChargerCSV
+            // 
+            this.ChargerCSV.FileName = "ChargerCSV";
+            this.ChargerCSV.FileOk += new System.ComponentModel.CancelEventHandler(this.ChargerCSV_FileOk);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChargerCSV);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "PlotThosesLines";
@@ -69,6 +87,8 @@
         #endregion
 
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnChargerCSV;
+        private System.Windows.Forms.OpenFileDialog ChargerCSV;
     }
 }
 
