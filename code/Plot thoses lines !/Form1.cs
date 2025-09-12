28309b08c205f4ea2562cbc32768a5127f24f891
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-12 14:37:23 +0200

    Feat (Data) : Des données peuvent etre afficher
    [30][DONE]
    Donnée ecrite en dur dans Form1.cs

--- code/Plot thoses lines !/Form1.cs
+++ code/Plot thoses lines !/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Globalization;
+using System.IO;
 
 namespace Plot_thoses_lines__
 {
@@ -15,9 +18,39 @@ namespace Plot_thoses_lines__
         public Form1()
         {
             InitializeComponent();
+
+            // Création d'une série temporelle
+            var serie = new Series("Mesures");
+            serie.ChartType = SeriesChartType.Line; // Ligne pour le temps
+            serie.XValueType = ChartValueType.DateTime; // Axe X = DateTime
+
+            // Ajouter quelques données fictives
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-4), 10);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-3), 20);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-2), 15);
+            serie.Points.AddXY(DateTime.Now.AddMinutes(-1), 25);
+            serie.Points.AddXY(DateTime.Now, 18);
+
+            // Ajouter la série au Chart
+            chart1.Series.Clear();
+            chart1.Series.Add(serie);
+
+            // Configurer les axes
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "HH:mm:ss"; // Format des heures
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+
+            // Optionnel : défilement/zoom
+            chart1.ChartAreas[0].AxisX.ScrollBar.Enabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
         {
 
         }
