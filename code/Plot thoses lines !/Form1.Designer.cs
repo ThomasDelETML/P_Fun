4273c6813b66ff9471f0c41815b6d0b235454cf8
Thomas <163824101+ThomasDelETML@users.noreply.github.com>
2025-09-26 14:34:32 +0200

    UX(Code) : changer l'apparence de l'app pour mieux voir les données
    [10][DONE]

--- code/Plot thoses lines !/Form1.Designer.cs
+++ code/Plot thoses lines !/Form1.Designer.cs
@@ -28,36 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnChargerCSV = new System.Windows.Forms.Button();
             this.ChargerCSV = new System.Windows.Forms.OpenFileDialog();
             this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // chart1
-            // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(-4, 59);
-            this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(805, 392);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
-            this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // btnChargerCSV
             // 
-            this.btnChargerCSV.Location = new System.Drawing.Point(686, 12);
+            this.btnChargerCSV.Location = new System.Drawing.Point(678, 12);
             this.btnChargerCSV.Name = "btnChargerCSV";
             this.btnChargerCSV.Size = new System.Drawing.Size(102, 37);
             this.btnChargerCSV.TabIndex = 1;
@@ -73,9 +51,9 @@
             // formsPlot1
             // 
             this.formsPlot1.DisplayScale = 0F;
-            this.formsPlot1.Location = new System.Drawing.Point(-4, 59);
+            this.formsPlot1.Location = new System.Drawing.Point(-1, 59);
             this.formsPlot1.Name = "formsPlot1";
-            this.formsPlot1.Size = new System.Drawing.Size(805, 392);
+            this.formsPlot1.Size = new System.Drawing.Size(993, 548);
             this.formsPlot1.TabIndex = 2;
             this.formsPlot1.Load += new System.EventHandler(this.formsPlot1_Load);
             // 
@@ -83,21 +61,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(992, 606);
             this.Controls.Add(this.formsPlot1);
             this.Controls.Add(this.btnChargerCSV);
-            this.Controls.Add(this.chart1);
             this.Name = "Form1";
             this.Text = "PlotThosesLines";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button btnChargerCSV;
         private System.Windows.Forms.OpenFileDialog ChargerCSV;
         private ScottPlot.WinForms.FormsPlot formsPlot1;
